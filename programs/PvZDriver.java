ac77109314def4e0a56a3e6a75348cd62a70113b
luigirivera <luigirivera@users.noreply.github.com>
2017-06-26 19:34:43 +0800

    Create PvZDriver.java

---
+++ programs/PvZDriver.java
@@ -0,0 +1,16 @@
+public class PvZDriver
+{
+	public static void main(String[] args)
+	{
+		int timer = 180;
+		
+		
+	  do{
+		
+		//game to be added
+		}while(timer!=0 && /*something for when owner dies*/);
+		
+		if(timer==0) System.out.println("TIME RAN OUT! YOU WIN!!");
+		else System.out.println("THE ZOMBIES ATE YOUR BRAIN! YOU LOSE!!");
+	}
+}
